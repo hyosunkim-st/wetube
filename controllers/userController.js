2c51107409918c349617c0dce19687e578296084
hyosun <q3020302@gmail.com>
2020-06-11 21:28:26 +0900

    Github Log In part One

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,8 +40,14 @@ export const getLogin = (req, res) =>
     successRedirect: routes.home 
   });
 
+export const githubLogin =
+
+export const githubLoginCallback = (accessToken, refreshToken, profile, cb) => {
+  console.log(accessToken, refreshToken, profile, cb);
+};
+
 export const logout = (req, res) => {
-  // To Do: Process Log Out
+  req.logout();
   res.redirect(routes.home);
 };
 export const userDetail = (req, res) =>
