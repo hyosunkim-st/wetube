e8a7405ffe65cbeca5f28ce3255c757ec48e000b
hyosun <q3020302@gmail.com>
2020-06-17 02:57:55 +0900

    recording

--- app.js
+++ app.js
@@ -20,21 +20,26 @@ const app = express();
 const CokieStore = MongoStore(session);
 
 app.use(helmet());
+app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan("dev"));
-app.use(session({
+
+app.use
+    (session({
     secret: process.env.COOKIE_SECRET,
     resave: true,
     saveUninitialized: false, 
     store: new CokieStore({mongooseConnection: mongoose.connection })
-}))
+    })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(localsMiddleware);
-
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
